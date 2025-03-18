ff589d7f93787bfe48c204e3e106e3e9c66dccc8
Phạm Nguyên Khoa <pnkvlog1508@gmail.com>
2025-03-18 15:30:47 +0700

    d

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { RequestMethod, ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
   app.setGlobalPrefix('api/v1', {
     exclude: [{ path: '', method: RequestMethod.GET }],
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: false
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.init();
+  return server;
 }
-bootstrap();
+
+export default bootstrap();
